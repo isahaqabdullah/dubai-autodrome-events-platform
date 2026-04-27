96bac15beba86d0c4220ed1e9aeae3eb4e6621b8
Isahaq Abdullah <isahaqabdullah@gmail.com>
2026-04-27 15:39:19 +0400

    Skip N-Genius confirmation page

--- services/ngenius.ts
+++ services/ngenius.ts
@@ -156,7 +156,8 @@ export async function createNgeniusOrder(input: NgeniusCreateOrderInput): Promis
     },
     merchantAttributes: {
       redirectUrl,
-      cancelUrl
+      cancelUrl,
+      skipConfirmationPage: true
     },
     merchantOrderReference: input.merchantOrderReference,
     merchantDefinedData: {
